78afee4f6ec486805f3833b08c0db8ba2514afc4
Roy Hsu <roy.hsu@tinyworld.cc>
2019-11-18 16:45:27 +0800

    add missing files.

---
+++ Sources/TinyCombine/TinyCombine.h
@@ -0,0 +1,13 @@
+//
+//  TinyCombine.h
+//  TinyCombine
+//
+//  Created by Roy Hsu on 18/11/2019.
+//  Copyright © 2019 TinyWorld. All rights reserved.
+//
+
+@import Foundation;
+
+FOUNDATION_EXPORT double TinyCombineVersionNumber;
+
+FOUNDATION_EXPORT const unsigned char TinyCombineVersionString[];
